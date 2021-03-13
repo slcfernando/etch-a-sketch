37a8adcbcbed6ea4f2e0f0aebe4fd7002c0575f8
Lance Fernando <sidneylance831@gmail.com>
2021-03-13 21:12:19 +0800

    Add basic grid functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+A sketchpad or etch-a-sketch implementation. Made for [The Odin Project](https://www.theodinproject.com/).
--- script.js
+++ script.js
@@ -7,6 +7,19 @@ for (let i = 0; i < 16; i++) {
         let pixel = document.createElement("div");
         pixel.className = "pixel";
         pixel.textContent = `${i}-${j}`;
+        pixel.style.backgroundColor = "rgb(255, 255, 255)";
         grid.appendChild(pixel);
     }
 }
+
+let pixels = document.querySelectorAll(".pixel");
+pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseenter", () => {
+        console.log(pixel.style.backgroundColor);
+        if (pixel.style.backgroundColor == "rgb(255, 255, 255)") {
+            pixel.style.backgroundColor = "rgb(210, 210, 210)";
+        } else {
+            pixel.style.backgroundColor = "rgb(255, 255, 255)";
+        }
+    })
+})
