b9343502c25e738937545181328765c334ded6d0
Lance Fernando <sidneylance831@gmail.com>
2021-03-18 19:06:30 +0800

    Improve appearance and simplify code

--- script.js
+++ script.js
@@ -7,25 +7,37 @@ const rainbowButton = document.querySelector("#rainbow")
 const bwButton = document.querySelector("#bw")
 let size = 16;  // default size
 let pixels;
+let canvasMode = "CLASSIC";  // default mode
 
 createCanvas(size);
-setClassicMode();
+setCanvasMode("CLASSIC");
 
 resizeButton.addEventListener("click", () => {
     do {
         size = prompt("Enter square grid side length:");
-        if (size < 10 || size > 80) {
+        if (size === null) {
+            break;
+        } else if (size < 10 || size > 80) {
             alert("Please enter from 10-80 only.");
-        }
+        } 
     } while (size < 10 || size > 80);
 
-    deleteCanvas();
-    createCanvas(size);
+    if (size) {
+        deleteCanvas();
+        createCanvas(size);
+        setCanvasMode("CLASSIC");
+    }
 })
 
-classicButton.addEventListener("click", setClassicMode);
-rainbowButton.addEventListener("click", setRainbowMode);
-bwButton.addEventListener("click", setBWMode);
+classicButton.addEventListener("click", () => {
+    setCanvasMode("CLASSIC");
+});
+rainbowButton.addEventListener("click", () => {
+    setCanvasMode("RAINBOW");
+});
+bwButton.addEventListener("click", () => {
+    setCanvasMode("BW");
+});
 
 function createCanvas(sideLength) {
     for (let i = 0; i < sideLength; i++) {
@@ -44,37 +56,63 @@ function deleteCanvas() {
     pixels.forEach((pixel) => pixel.remove());
 }
 
-function setClassicMode() {
-    deleteCanvas();
-    createCanvas(size);
-    pixels.forEach((pixel) => pixel.addEventListener("mouseenter", () => {
-        if (pixel.style.backgroundColor == "rgb(255, 255, 255)") {
-            pixel.style.backgroundColor = "rgb(210, 210, 210)";
-        } else {
-            pixel.style.backgroundColor = "rgb(255, 255, 255)";
-        }
-    }));
+function setCanvasMode(mode) {
+    removeCanvasEventListeners();
+    clearCanvas();
+    classicButton.disabled = false;
+    rainbowButton.disabled = false;
+    bwButton.disabled = false;
+
+    if (mode == "CLASSIC") {
+        canvasMode = "CLASSIC";
+        pixels.forEach((pixel) => pixel.addEventListener("mouseenter", setPixelClassicMode));
+        classicButton.disabled = true;
+    } else if (mode == "RAINBOW") {
+        canvasMode = "RAINBOW";
+        pixels.forEach((pixel) => pixel.addEventListener("mouseenter", setPixelRainbowMode));
+        rainbowButton.disabled = true;
+    } else if (mode == "BW") {
+        canvasMode = "BW";
+        pixels.forEach((pixel) => pixel.addEventListener("mouseenter", setPixelBWMode));
+        bwButton.disabled = true;
+    }
 }
 
-function setRainbowMode() {
-    deleteCanvas();
-    createCanvas(size);
-    pixels.forEach((pixel) => pixel.addEventListener("mouseenter", () => {
-        pixel.style.backgroundColor = `rgb(${Math.floor((Math.random() * 256))}, ${Math.floor((Math.random() * 256))}, ${Math.floor((Math.random() * 256))})`;
-    }));
+function setPixelClassicMode(event) {
+    let pixel = event.target;
+    if (pixel.style.backgroundColor == "rgb(255, 255, 255)") {
+        pixel.style.backgroundColor = "rgb(210, 210, 210)";
+    } else {
+        pixel.style.backgroundColor = "rgb(255, 255, 255)";
+    }
 }
 
-function setBWMode() {
-    deleteCanvas();
-    createCanvas(size);
-    pixels.forEach((pixel) => pixel.addEventListener("mouseenter", () => {
-        let rgbLength = pixel.style.backgroundColor.length;
-        let rgbNumbers = pixel.style.backgroundColor.substring(4,rgbLength-1).split(", ");
-        console.log(rgbNumbers);
-        if (rgbNumbers[0] == 3) {  // if the rgb value is (3, 3, 3), it's reached the darkest possible shade
-            pixel.style.backgroundColor = "rgb(255, 255, 255)";
-        } else {
-            pixel.style.backgroundColor = `rgb(${rgbNumbers[0]-28}, ${rgbNumbers[1]-28}, ${rgbNumbers[2]-28})`;
-        }
-    }));
+function setPixelRainbowMode(event) {
+    let pixel = event.target;
+    pixel.style.backgroundColor = `rgb(${Math.floor((Math.random() * 256))}, ${Math.floor((Math.random() * 256))}, ${Math.floor((Math.random() * 256))})`;
+}
+
+function setPixelBWMode(event) {
+    let pixel = event.target;
+    let rgbLength = pixel.style.backgroundColor.length;
+    let rgbNumbers = pixel.style.backgroundColor.substring(4,rgbLength-1).split(", ");
+    if (rgbNumbers[0] == 3) {  // if the rgb value is (3, 3, 3), it's reached the darkest possible shade
+        pixel.style.backgroundColor = "rgb(255, 255, 255)";
+    } else {
+        pixel.style.backgroundColor = `rgb(${rgbNumbers[0]-28}, ${rgbNumbers[1]-28}, ${rgbNumbers[2]-28})`;
+    }
+}
+
+function removeCanvasEventListeners() {
+    if (canvasMode == "CLASSIC") {
+        pixels.forEach((pixel) => pixel.removeEventListener("mouseenter", setPixelClassicMode));
+    } else if (canvasMode == "RAINBOW") {
+        pixels.forEach((pixel) => pixel.removeEventListener("mouseenter", setPixelRainbowMode));
+    } else if (canvasMode == "BW") {
+        pixels.forEach((pixel) => pixel.removeEventListener("mouseenter", setPixelBWMode));
+    }
+}
+
+function clearCanvas() {
+    pixels.forEach((pixel) => pixel.style.backgroundColor = "rgb(255, 255, 255)");
 }
